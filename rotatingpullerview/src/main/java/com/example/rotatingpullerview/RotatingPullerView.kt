5632be37169496d2e301056d7e59042ce5c1eb16
Anwesh43 <anweshthecool0@gmail.com>
2021-05-02 00:59:42 +0530

    adding Float and Int extension methdos

--- rotatingpullerview/src/main/java/com/example/rotatingpullerview/RotatingPullerView.kt
+++ rotatingpullerview/src/main/java/com/example/rotatingpullerview/RotatingPullerView.kt
@@ -10,11 +10,11 @@ import android.app.Activity
 import android.content.Context
 
 val colors : Array<Int> = arrayOf(
-    "",
-    "",
-    "",
-    "",
-    ""
+    "#f44336",
+    "#3F51B5",
+    "#FF9800",
+    "#006064",
+    "#311B92"
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
@@ -25,3 +25,8 @@ val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val blockFactor : Float = 0.2f
 val hFactor : Float = 4.9f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
